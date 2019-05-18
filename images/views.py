ec8ffdbb693f79cf18bd589d88c03c149d8b2708
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-18 22:47:01 +0300

    request method

--- images/views.py
+++ images/views.py
@@ -4,9 +4,21 @@ import datetime as dt
 # Create your views here.
 def home(request):
   images = image.get_images()
-  comments = comment.get_comment()
+  comments = Comment.get_comment()
   profile = profile.get_profile()
 
-  
+
+  current.user = request.user
+  if request.method == 'POST':
+    form = CommentForm(request.post)
+    if form.is valid():
+      comment = form.save(commit=False)
+      comment.save()
+    return redirect('home')
+
+  else:
+    form = CommetnForm()
+
+  return render(request,"home.html",{"images":images,"comments":comments,"form":form,"profile":profile})
    
      
