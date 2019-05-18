55a6c3dab5c9347ec947e7afae04a3b25330b8c1
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-18 22:41:01 +0300

    home function in views.py file

--- images/models.py
+++ images/models.py
@@ -5,3 +5,19 @@ class Profile(models.Model):
   usesr = models.OneToOneField(User , on_delete=models.CASCADE,null=True)
   profile_photo=models.ImageField(upload_to='profiles/',null=True)
   bio=models,charField(max_length=240,null=True)
+
+
+  def save_profile(self):
+    self.save()
+
+
+  @classmethod
+  def get_profile(cls):
+    profile = profile.objects.all()
+    return profile 
+
+
+  @classmethod
+  def find_profile(cls,search_term):
+    profile = profile.objects.filter(user__username__icontains=search_term)
+    return profile 
--- images/views.py
+++ images/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404,HttpResponseRedirect  
 import datetime as dt 
 # Create your views here.
-def welcome(request):
-  return HttpResponse('welcome to the moringa tribune ')
- 
+def home(request):
+  images = image.get_images()
+  comments = comment.get_comment()
+  profile = profile.get_profile()
+
+  
    
      
