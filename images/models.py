d4ae1b18c2c9fdaec751182ce82b74d48b80f289
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-18 22:30:45 +0300

    profile class having the username,photo and bio

--- images/models.py
+++ images/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import user 
 # Create your models here.
+class Profile(models.Model):
+  usesr = models.OneToOneField(User , on_delete=models.CASCADE,null=True)
+  profile_photo=models.ImageField(upload_to='profiles/',null=True)
+  bio=models,charField(max_length=240,null=True)
