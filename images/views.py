aa4ba0d5a27259901cb1d09db64df8eeae2a56f9
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-19 22:17:40 +0300

    login required,index

--- images/views.py
+++ images/views.py
@@ -1,30 +1,6 @@
 from django.shortcuts import render,redirect 
-from django.http import HttpResponse,Http404,HttpResponseRedirect  
-import datetime as dt 
+ 
 # Create your views here.
-def home(request):
-  images = image.get_images()
-  comments = Comment.get_comment()
-  profile = profile.get_profile()
-
-
-  current.user = request.user
-  if request.method == 'POST':
-    form = CommentForm(request.post)
-    if form.is valid():
-      comment = form.save(commit=False)
-      comment.save()
-    return redirect('home')
-
-  else:
-    form = CommetnForm()
-
-  return render(request,"home.html",{"images":images,"comments":comments,"form":form,"profile":profile})
-def profile(request.profile_id):
-
-  profile = profile.objects.get(pk = profile_id)
-  images = image.objects.filter(profile_id=profile).all()
-
-  return render(request,"profile.html",{"profile":profile,"images":images})
+ 
    
      
