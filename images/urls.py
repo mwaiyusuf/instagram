08291cf234ed19ab897c32b6403599b5d742d4da
mwaiyusuf <mwaigalo5@gmail.com>
2019-05-20 05:24:11 +0300

    profile and notification

--- images/urls.py
+++ images/urls.py
@@ -1,4 +1,14 @@
 from django.conf.urls import url 
+from . import views
+from django.conf import settings
+from django.shortcuts import render, redirect
 # from . import views 
 
  
+urlpatterns=[
+  
+
+
+
+
+]
--- images/views.py
+++ images/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render,redirect 
- 
+ from django.http import HttpResponse
+from django.conf.urls import url,include
+from django.contrib.auth import authenticate, login, logout
+from .forms import PostForm
+from .models import Profile, Image
+from django.contrib.auth.models import User
+from . import models
 # Create your views here.
  
    
